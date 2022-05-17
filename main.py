7b3b1997a9c3c4f639205d0a0be7d5bbbfd0cbaf
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-05-17 17:04:49 -0400

    Building code

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-# This is a sample Python script.
+import os
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    input()
-
-
-# Press the green button in the gutter to run the script.
+# Method Main
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    ContinueP = "S"
+    print("Welcome to the SSN Validator")  # Welcome Message
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while ContinueP == "S":
+        print("Press 'Enter' to continue...")  # Request to press enter after presenting the welcome message.
+        stay = True
+        WaitP = input()
+        if WaitP != "":
+            print("Press 'Enter' to continue...")
+            ContinueP = "S"
+            os.system("cls")
+        else:
+            print("Digit 9 digits of Social Security Number: ")  # Validations 9 digits
+            SSN = input()
+            # If SSN is null
+            if not SSN:
+                ContinueP = "S"
+                os.system("cls")
+                print("Please, complete the data entry")
+            else:
+                if SSN.replace('-', '').isnumeric():  # Replace (-) for moment
+                    ListDigit = SSN.split('-', 4)
+                    if len(ListDigit[0]) == 3:  # Validation for the first group
+                        if int(ListDigit[0]) == 000 or int(ListDigit[0]) == 666 or 900 <= int(ListDigit[0]) <= 999:
+                            print("The number is invalid")
+                        else:
+                            if 1 < int(ListDigit[1]) < 99:
+                                if 1 < int(ListDigit[2]) < 9999:
+                                    print("The SSN " + SSN + " is aa valid social security number")
+                                else:
+                                    print("The number of digit can't to be diferent that 4 on the last group")
+                            else:
+                                print("The number of digit can't be diferent that 2 on the second group")
+                    else:
+                        print("The number of digit can't be diferent that 3 on the first group")
+                else:
+                    print("Invalid Entry")
+
+            # Validation Last Entry
+        while stay:
+            print("Do you want to continue on the program? press (S) else press any key")
+            ContinueP = input().upper()
+
+            if not ContinueP:
+                print("Need to digit any number")
+                stay = True
+            elif ContinueP.isnumeric():
+                stay = True
+            else:
+                stay = False
